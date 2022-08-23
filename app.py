da5ca4e4411a8cfb770c9d4e0bd1ba32209682bc
Kalla kruparaju <64892543+kallakruparaju@users.noreply.github.com>
2022-08-24 01:59:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Khatam Tata Bye Bye BYE BYE BYE Gd bye!!!</h1>'
+
+app.run(host='0.0.0.0',port=5000)
