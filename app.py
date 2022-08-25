9c19a13cec7e8c6ac85a2c5d9f80cec7e16d6b26
Kalla kruparaju <64892543+kallakruparaju@users.noreply.github.com>
2022-08-25 16:54:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Khatam BYE BYE  test Bye BYE BYE BYE Gd bye!!!</h1>'
+    return '<h1> BYE BYE  test Bye BYE BYE BYE Gd bye!!!</h1>'
 
 app.run(host='0.0.0.0',port=5000)
