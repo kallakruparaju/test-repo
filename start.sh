6f020d94e3f4f2a3cb47372deed0c50b9c52a985
Kalla kruparaju <64892543+kallakruparaju@users.noreply.github.com>
2022-08-25 16:42:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python3 app.py
+sudo netstat -tnlp | grep 5000
+if [ $(echo $?) = 1 ]
+then
+  	screen -m -d python3 app.py
+else
+      fuser -k 5000/tcp
+      screen -m -d python3 app.py
+fi
